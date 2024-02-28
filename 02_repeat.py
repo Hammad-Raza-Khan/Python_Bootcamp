ec9aea4297a16d068a42957cad5c0cac12b2729e
Hammad Raza Khan <hammadrazakhann@gmail.com>
2024-02-28 22:04:14 +0530

    Add files via upload

---
+++ 02_repeat.py
@@ -0,0 +1,39 @@
+class H():
+    def __init__(self):
+        print("Hey ")
+    def __country__(self):
+        return " Palestine !"
+    def combine(self):
+        return "Free"
+        
+value=H()
+
+
+
+
+class OG(H):
+    def __init__(se):
+        H.__init__(se)
+        print( "Insha'Allah" )
+        
+    def self(se)   :
+        return "Soon!"
+        
+unity=OG()
+print(unity.combine()+ unity.__country__())
+
+print(unity.self())
+
+
+class salam():
+    def __init__(self, name):
+        salam.naaam=name
+        
+    def display(self):
+        print("Assalam Alaikum " + salam.naaam)
+        
+        
+ui=salam("Hammad")
+print(ui.display())
+        
+        
